88c0451d4ddc4ef46035f1eff33555b13aca185d
harsh <harsh08.walia@gmail.com>
2025-02-17 17:08:08 -0500

    added reverse string program

---
+++ programs/reverseString.py
@@ -0,0 +1,33 @@
+# Program to reverse a string without slicing
+string = "This is Python Programming"
+
+def createStringList():
+    return string.split(" ")  
+
+def reverse(listString):
+    reverseStringList = []  
+    for element in listString:
+        reverseString = ""  
+        reverseIndex = len(element) - 1
+        while reverseIndex >= 0:
+            reverseString += element[reverseIndex]  
+            reverseIndex -= 1
+        reverseStringList.append(reverseString) 
+    return reverseStringList
+
+def reverseString(stringList):
+    reversed_sentence = ""  
+    index = len(stringList) - 1  
+    while index >= 0:
+        reversed_sentence += stringList[index] + " "  
+        index -= 1
+    return reversed_sentence 
+
+splitString = createStringList()
+print("Original Words:", splitString)
+
+reversedWordsList = reverse(splitString)
+print("Reversed Words List:", reversedWordsList)
+
+finalReversedString = reverseString(reversedWordsList)
+print("Final Reversed String:", finalReversedString)
